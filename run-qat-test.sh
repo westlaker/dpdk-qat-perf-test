06098d3f04c1d37e2496b3b59286c45ef4a29d73
JC G <westlakers@gmail.com>
2024-03-01 19:33:37 +0000

    update run-qat-test.sh

--- run-qat-test.sh
+++ run-qat-test.sh
@@ -58,6 +58,24 @@ case $1 in
 	2)
 
 		echo "sudo ${DPDK_TEST_CRYPTO_PERF}/dpdk-test-crypto-perf \
+		  -l 4-7 -a ${QAT_PF0}.0 \
+		  --vdev crypto_aesni_mb \
+		  -- --devtype crypto_aesni_mb \
+		  --buffer-sz 64,128,256,512,1024,2048 --cipher-algo aes-cbc \
+		  --cipher-key-sz 16 --cipher-iv-sz 16 --cipher-op encrypt --optype cipher-only \
+		  --burst-sz 32 --ptest throughput --total-ops 30000000 "
+
+		sudo ${DPDK_TEST_CRYPTO_PERF}/dpdk-test-crypto-perf \
+		  -l 2-3 -a ${QAT_PF0}.0 \
+		  --vdev crypto_aesni_mb \
+		  -- --devtype crypto_aesni_mb \
+		  --buffer-sz 64,128,256,512,1024,2048 --cipher-algo aes-cbc \
+		  --cipher-key-sz 16 --cipher-iv-sz 16 --cipher-op encrypt --optype cipher-only \
+		  --burst-sz 32 --ptest throughput --total-ops 30000000
+		;;
+
+	3)
+		echo "sudo ${DPDK_TEST_CRYPTO_PERF}/dpdk-test-crypto-perf \
 		  -l 2-3 -a ${QAT_PF0}.0 \
 		  --vdev crypto_aesni_mb \
 		  -- --devtype crypto_aesni_mb \
@@ -74,28 +92,8 @@ case $1 in
 		  --buffer-sz 64,128,256,512,1024,2048 --cipher-algo aes-cbc \
 		  --cipher-key-sz 16 --cipher-iv-sz 16 --cipher-op encrypt --optype cipher-then-auth \
 		  --auth-op generate --digest-sz 12 --auth-algo sha1-hmac --auth-key-sz 64 \
-		  --burst-sz 32 --ptest throughput --total-ops 30000000 
 		;;
-	3)
 
-		echo "sudo ${DPDK_TEST_CRYPTO_PERF}/dpdk-test-crypto-perf \
-		  -l 4-7 -a ${QAT_PF0}.0 \
-		  --vdev crypto_aesni_mb1 \
-		  --vdev crypto_aesni_mb2 \
-		  -- --devtype crypto_aesni_mb \
-		  --buffer-sz 64,128,256,512,1024,2048 --cipher-algo aes-cbc \
-		  --cipher-key-sz 16 --cipher-iv-sz 16 --cipher-op encrypt --optype cipher-only \
-		  --burst-sz 32 --ptest latency --total-ops 30 "
-
-		sudo ${DPDK_TEST_CRYPTO_PERF}/dpdk-test-crypto-perf \
-		  -l 4-7 -a ${QAT_PF0}.0 \
-		  --vdev crypto_aesni_mb1 \
-		  --vdev crypto_aesni_mb2 \
-		  -- --devtype crypto_aesni_mb \
-		  --buffer-sz 64,128,256,512,1024,2048 --cipher-algo aes-cbc \
-		  --cipher-key-sz 16 --cipher-iv-sz 16 --cipher-op encrypt --optype cipher-only \
-		  --burst-sz 32 --ptest latency --total-ops 30
-		;;
 	4)
 
 		echo "sudo ${DPDK_TEST_CRYPTO_PERF}/dpdk-test-crypto-perf \
